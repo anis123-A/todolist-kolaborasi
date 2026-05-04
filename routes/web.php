5c887e44241c0421ead575cf5c1055217561fff6
anis <annissafitri31@gmail.com>
2026-05-04 12:12:32 +0800

    feat: aplikasi todolist selesai dan berjalan lancar

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
 
-// Jalur untuk menampilkan data (Read)
+// Jalur untuk menampilkan data (Tugas Salmia)
 Route::get('/', [Controller::class, 'index'])->name('tasks.index');
 
-// Jalur untuk menghapus data (Delete)
+// Jalur untuk menyimpan data (Tugas Annis)
+Route::post('/tasks', [Controller::class, 'store'])->name('tasks.store');
+
+// Jalur untuk menghapus data (Tugas Salmia)
 Route::delete('/tasks/{id}', [Controller::class, 'destroy'])->name('tasks.destroy');
